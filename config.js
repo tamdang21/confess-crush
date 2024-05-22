321246781c546c474e1890cb9203f1f0d685ce10
tamdang21 <dtamhoang21@gmail.com>
2024-05-22 10:23:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'bởi vì Dung rất thích a Tâm <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Nhớ để ý ny của be nh vào🤞😊.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100026987723953&mibextid=ZbWKwL' 
 }
